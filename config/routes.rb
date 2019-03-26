a031c871ff364be7cb712df2c0a3b611399ed065
Maria <maria.altyeva@gmail.com>
2019-03-26 10:58:06 +0800

    add restaurants crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants
+  root to: "restaurants#index"
+
+  # #Read all restaurants
+  # get '/restaurants', to: "restaurants#index"
+
+  # #Read one restaurant
+  # #/restaurants/45
+  # get '/restaurants/:id', to: "restaurants#show"
+
+  # #Create one restaurant
+
+  # #1st to get the blank form
+  # get '/restaurants/new', to: "restaurants#new"
+
+  # #2nd to save in the database
+  # post '/restaurants', to: "restaurants#create"
+
+  # #Update one restaurant
+
+  # #1st get the restaurant form
+  # #/restaurants/56/edit
+  # get '/restaurants/:id/edit', to: "restaurants#edit"
+  # #2nd save in DB
+  # patch '/restaurants/:id', to: "restaurants#update"
+
+  # #Destroy one restaurant
+  # delete '/restaurants/:id', to: "restaurants#destroy"
+
+
 end
